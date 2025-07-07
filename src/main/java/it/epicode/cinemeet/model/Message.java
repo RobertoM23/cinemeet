3548f5f538d67b5b0a2cadca11f140877d3d1431
Roberto Marino <197581795+RobertoM23@users.noreply.github.com>
2025-07-07 09:37:57 +0200

    Message

---
+++ src/main/java/it/epicode/cinemeet/model/Message.java
@@ -0,0 +1,50 @@
+package it.epicode.cinemeet.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Event event;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+}
