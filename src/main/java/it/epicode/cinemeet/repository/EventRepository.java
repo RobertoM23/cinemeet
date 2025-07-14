5fe047afac337420057215ab771d7703714f7db8
Roberto Marino <197581795+RobertoM23@users.noreply.github.com>
2025-07-14 10:56:10 +0200

    Update EventRepository.java

--- src/main/java/it/epicode/cinemeet/repository/EventRepository.java
+++ src/main/java/it/epicode/cinemeet/repository/EventRepository.java
@@ -8,4 +8,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByCreatorId(Long creatorId);
     List<Event> findByCreatorIdNot(Long creatorId);
+    List<Event> findByCinema(String cinema);
+    List<Event> findByDate(String date);
+    List<Event> findByMovieTitleContainingIgnoreCase(String movieTitle);
 }
